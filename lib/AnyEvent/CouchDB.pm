137af04699dff01b8991e48ce6ffc9d2c2c20093
John Beppu <beppu@work.(none)>
2008-07-17 12:10:17 -0700

    more docs

--- lib/AnyEvent/CouchDB.pm
+++ lib/AnyEvent/CouchDB.pm
@@ -8,6 +8,7 @@ use JSON::XS;
 use AnyEvent::HTTP;
 use AnyEvent::CouchDB::Database;
 use URI::Escape;
+use Data::Dump 'pp';
 
 our $cvcb = sub {
   my ($options, $status) = @_;
@@ -30,7 +31,7 @@ our $cvcb = sub {
     my ($body, $headers) = @_;
     my $response;
     eval { $response = decode_json($body); };
-    $cv->croak(pp(['decode_error', $@, $body, encode_json($headers)])) if ($@);
+    $cv->croak(pp(['decode_error', $@, $body, $headers])) if ($@);
     if ($headers->{Status} == $status) {
       $success->($response);
     } else {
@@ -89,18 +90,32 @@ AnyEvent::CouchDB - a non-blocking CouchDB client based on jquery.couch.js
 
 =head1 SYNOPSIS
 
+Getting information about a CouchDB server:
+
   use AnyEvent::CouchDB;
   use Data::Dump 'pp';
 
-  my $couch = AnyEvent::CouchDB->new('http://localhost:5984/');
+  my $couch = AnyEvent::CouchDB->new;
   print pp( $couch->all_dbs->recv ), "\n";
   print pp( $couch->info->recv    ), "\n";
+
+Get an object representing a CouchDB database:
 
   my $db = $couch->db('database');
 
 =head1 DESCRIPTION
 
-AnyEvent::CouchDB is a non-blocking CouchDB client based on jquery.couch.js.
+AnyEvent::CouchDB is a non-blocking CouchDB client implemented on top of the
+L<AnyEvent> framework.  Using this library will give you the ability to run
+many CouchDB requests asynchronously.  However, it can also be used synchronously
+if you want.
+
+Its API is based on jquery.couch.js, but we've adapted the API slightly so that
+it makes sense in an asynchronous Perl environment.
+
+The main thing you have to remember is that all the data retrieval methods
+return an AnyEvent condvar, C<$cv>.  If you want the actual data from the
+request, it's up to you to call C<recv> on it.
 
 =head1 API
 
@@ -108,16 +123,74 @@ AnyEvent::CouchDB is a non-blocking CouchDB client based on jquery.couch.js.
 
 =head3 $couch = AnyEvent::CouchDB->new([ $url ])
 
+This method will instantiate an object that represents a CouchDB server.
+By default, it connects to L<http://localhost:5984>, but you may explicitly
+give it another URL if you want to connect to a CouchDB server on another
+server and/or a non-default port.
+
 =head3 $db = $couch->db($name)
+
+This method takes a name and returns an L<AnyEvent::CouchDB::Database> object.
+This is the object that you'll use to work with CouchDB documents.
 
 =head2 Queries and Actions
 
 =head3 $cv = $couch->all_dbs([ \%options ])
 
+This method requests an arrayref that contains the names of all the databases
+hosted on the current CouchDB server.  It returns an AnyEvent condvar that
+you'll be expected to call C<recv> on to get the data back.
+
 =head3 $cv = $couch->info([ \%options ])
+
+This method requests a hashref of info about the current CouchDB server and
+returns a condvar that you should call C<recv> on.  The hashref that's returned
+looks like this:
+
+  {
+    couchdb => 'Welcome',
+    version => '0.7.3a658574'
+  }
 
 =head3 $cv = $couch->replicate($source, $target, [ \%options ])
 
+This method requests that a C<$source> CouchDB database be replicated to a
+C<$target> CouchDB database.  To represent a local database, pass in just
+the name of the database.  To represent a remote database, pass in the
+URL to that database.  Note that both databases must already exist for
+the replication to work.
+
+B<Examples>:
+
+  # local to local
+  $couch->replicate('local_a', 'local_a_backup')->recv;
+
+  # local to remote
+  $couch->replicate('local_a', 'http://elsewhere/remote_a')->recv
+
+  # remote to local
+  $couch->replicate('http://elsewhere/remote_a', 'local_a')->recv
+
+As usual, this method returns a condvar that you're expected to call C<recv> on.
+Doing this will return a hashref that looks something like this upon success:
+
+  {
+    history => [
+      {
+        docs_read       => 0,
+        docs_written    => 0,
+        end_last_seq    => 149,
+        end_time        => "Thu, 17 Jul 2008 18:08:13 GMT",
+        missing_checked => 44,
+        missing_found   => 0,
+        start_last_seq  => 0,
+        start_time      => "Thu, 17 Jul 2008 18:08:13 GMT",
+      },
+    ],
+    ok => bless(do { \(my $o = 1) }, "JSON::XS::Boolean"),
+    session_id      => "cac3c6259b452c36230efe5b41259c04",
+    source_last_seq => 149,
+  }
 
 =head1 SEE ALSO
 
